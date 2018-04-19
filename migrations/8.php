19f1c227ba41a0cc52005c0d616dfd5a8a0115db
Pablo de la Peña <p@shedcollective.org>
2018-04-19 11:29:26 +0100

    Added migration for callback data

---
+++ migrations/8.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Migration:   8
+ * Started:     19/04/2018
+ * Finalised:   19/04/2018
+ *
+ * @package     Nails
+ * @subpackage  module-invoice
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Database\Migration\Nailsapp\ModuleInvoice;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration8 extends Base
+{
+    /**
+     * Execute the migration
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query("ALTER TABLE `{{NAILS_DB_PREFIX}}invoice_invoice_item` ADD `callback_data` TEXT NULL AFTER `grand_total`;");
+    }
+}
