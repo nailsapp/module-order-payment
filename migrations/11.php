4966584fa4406de0b37a71589311a2cd4269aaa4
Pablo de la Peña <p@shedcollective.org>
2019-07-08 12:42:42 +0100

    Increases size of invoice ref field

---
+++ migrations/11.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Migration:   11
+ * Started:     08/07/2019
+ *
+ * @package     Nails
+ * @subpackage  module-invoice
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Database\Migration\Nails\ModuleInvoice;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration11 extends Base
+{
+    /**
+     * Execute the migration
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query("ALTER TABLE `{{NAILS_DB_PREFIX}}invoice_invoice` CHANGE `ref` `ref` CHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';");
+    }
+}
