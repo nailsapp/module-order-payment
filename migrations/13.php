dfd5dea95d6796f6a4997b11829d4908b20fc9f1
Pablo de la Peña <p@shedcollective.org>
2019-10-03 12:52:53 +0100

    Adds support for cardholder name
    Addresses #78

---
+++ migrations/13.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Migration:   13
+ * Started:     03/10/2019
+ *
+ * @package     Nails
+ * @subpackage  module-invoice
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Database\Migration\Nails\ModuleInvoice;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration13 extends Base
+{
+    /**
+     * Execute the migration
+     *
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}invoice_source` ADD `name` VARCHAR(150) NULL DEFAULT NULL AFTER `label`;');
+    }
+}
