03617ea7b539d0d36a8098b77fd7c671307024ed
Pablo de la Peña <p@shedcollective.org>
2021-03-28 10:10:48 +0100

    Various
    - Add logging
    - Prevent double submission
    - Improve UX

--- assets/js/components/Refund.js
+++ assets/js/components/Refund.js
@@ -1,15 +1,35 @@
 class Refund {
+
+    /**
+     * Initialises the class
+     * @param adminController
+     */
     constructor(adminController) {
         this.adminController = adminController;
-        this.adminController.onRefreshUi(() => {
-            this.init();
-        });
+        this
+            .adminController
+            .log('Constructing')
+            .onRefreshUi(() => {
+                this.init();
+            });
     }
 
+    // --------------------------------------------------------------------------
+
+    /**
+     * Binds to new rfeund buttons
+     * @returns {Refund}
+     */
     init() {
-        $('.js-invoice-refund:not(.js-invoice-refund--processed)')
+        this.adminController.log('Looking for new items');
+        let $items = $('.js-invoice-refund:not(.js-invoice-refund--processed)');
+        this.adminController.log(`Found ${$items.length} new items`);
+
+        $items
             .addClass('js-invoice-refund--processed')
             .on('click', (e) => {
+
+                this.adminController.log('Opening modal');
 
                 e.preventDefault();
                 e.stopPropagation();
@@ -22,29 +42,61 @@ class Refund {
                     $item.data('max-formatted')
                 )
             });
+
+        return this;
     }
 
+    // --------------------------------------------------------------------------
+
+    /**
+     * Builds and shws the modal
+     * @param paymentId The payment ID to refund
+     * @param maxAmount The maximum refund amount permitted
+     * @param maxAmountFormatted The maximum refund amount permitted, as a string
+     * @returns {Refund}
+     */
     showModal(paymentId, maxAmount, maxAmountFormatted) {
 
-        let msg;
+        this.$text = $('<p>').text('Please confirm refund amount and reason:');
 
-        msg = 'Please confirm refund amount and reason:';
-        msg += `<br /><br /><input type="text" id="refund-amount" value="${maxAmount / 100}" style="width: 100%">`;
-        msg += '<div id="refund-amount-error" class="text-danger hidden" style="margin-bottom: 1em;"></div>';
-        msg += '<textarea id="refund-reason" placeholder="Reason for refund (optional)" style="width: 100%; margin: 0;"></textarea>';
+        this.$inputAmount = $('<input>')
+            .val(maxAmount / 100)
+            .css({
+                'width': '100%',
+                'margin-bottom': '1em'
+            });
 
-        let $modal = $('<div>')
-            .html(msg)
+        this.$amountError = $('<p>')
+            .addClass('text-danger hidden')
+            .css({
+                'margin-bottom': '1em'
+            });
+
+        this.$inputReason = $('<textarea>')
+            .attr('placeholder', 'Reason for refund (optional)')
+            .css({
+                'width': '100%',
+                'margin': 0
+            });
+
+        this.$modal = $('<div>')
+            .append(this.$text)
+            .append(this.$inputAmount)
+            .append(this.$amountError)
+            .append(this.$inputReason)
             .dialog({
                 'title': 'Are you sure?',
                 'resizable': false,
                 'draggable': false,
+                'closeable': false,
                 'modal': true,
                 'buttons': {
                     'OK': () => {
 
-                        let amount = $('#refund-amount').val();
-                        let reason = $('#refund-reason').val();
+                        let amount = this.$inputAmount.val();
+                        let reason = this.$inputReason.val();
+
+                        this.adminController.log('Clicked OK', amount, reason);
 
                         if (amount <= 0) {
                             this.setErrorMessage('Refund amount must be greater than 0.');
@@ -53,28 +105,71 @@ class Refund {
                             this.setErrorMessage(`Refund amount must be no greater than ${maxAmountFormatted}.`);
 
                         } else {
-
-                            this.clearErrorMessage();
-
-                            let $form = this.buildForm(paymentId, amount, reason);
-
-                            $('body').append($form);
-
-                            $form.submit();
-                            $modal.dialog('close');
+                            this
+                                .clearErrorMessage()
+                                .buildForm(paymentId, amount, reason)
+                                .submitForm();
                         }
                     },
                     'Cancel': () => {
-                        $modal.dialog('close');
+                        this.closeModal();
                     }
                 }
             });
+
+        return this;
     }
 
     // --------------------------------------------------------------------------
 
+    /**
+     * Submits the form
+     * @returns {Refund}
+     */
+    submitForm() {
+        this.adminController.log('Submitting form');
+        this.$modal.dialog('close');
+        this.$modal = $('<div>')
+            .html('<p>Submitting refund...</p>')
+            .dialog({
+                'title': 'Please wait',
+                'resizable': false,
+                'draggable': false,
+                'modal': true,
+                'dialogClass': 'no-close',
+            });
+        $('body').append(this.$form);
+        this.$form.submit();
+        return this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Closes the modal
+     * @returns {Refund}
+     */
+    closeModal() {
+        this.adminController.log('Closing modal');
+        this.$modal.dialog('close');
+        if (this.$form) {
+            this.$form.remove();
+            this.$form = null;
+        }
+        return this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Builds a form to submit
+     * @param paymentId
+     * @param amount
+     * @param reason
+     * @returns {Refund}
+     */
     buildForm(paymentId, amount, reason) {
-        return $('<form>')
+        this.$form = $('<form>')
             .attr({
                 'method': 'POST',
                 'action': `${window.SITE_URL}admin/invoice/payment/refund/${paymentId}`
@@ -91,12 +186,22 @@ class Refund {
                 'name': 'return_to',
                 'value': window.location.href
             }));
+
+        return this;
     }
 
     // --------------------------------------------------------------------------
 
+    /**
+     * Sets the error message
+     * @param msg
+     * @returns {Refund}
+     */
     setErrorMessage(msg) {
-        $('#refund-amount-error')
+
+        this.adminController.warn(msg);
+
+        this.$amountError
             .text(msg)
             .removeClass('hidden');
 
@@ -105,9 +210,16 @@ class Refund {
 
     // --------------------------------------------------------------------------
 
+    /**
+     * Clears any error messages
+     * @returns {Refund}
+     */
     clearErrorMessage() {
-        $('#refund-amount-error')
+
+        this.$amountError
             .addClass('hidden');
+
+        return this;
     }
 }
 
