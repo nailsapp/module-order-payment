279b675a239843aa1fdf729bce45c8ec4123dbb7
Pablo de la Peña <p@shedcollective.org>
2023-07-04 11:12:40 +0100

    feat: Static analysis fixes

---
+++ .phpstan/classes/App/Controller/Base.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+class Base
+{
+        public function __construct()
+        {
+        }
+}
---
+++ src/Exception/ChargeResponseException.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Charge Response Exception
+ *
+ * @package     Nails
+ * @subpackage  module-invoice
+ * @category    Exceptions
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Invoice\Exception;
+
+use Nails\Invoice\Exception\ResponseException;
+
+/**
+ * Class ChargeResponseException
+ *
+ * @package Nails\Invoice\Exception
+ */
+class ChargeResponseException extends ResponseException
+{
+}
